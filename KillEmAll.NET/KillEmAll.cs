2eb2b48df84b8f2ccdeaf153f5d35d187a1547a7
Nick Shaw <dev@d7xtech.com>
2021-04-27 10:19:24 -0400

    updated to use Unicode APIs, other minor improvements...

--- KillEmAll.NET/KillEmAll.cs
+++ KillEmAll.NET/KillEmAll.cs
@@ -10,29 +10,29 @@ namespace KillEmAll.NET
     class KillEmAll
     {
         private Dictionary<string, string> _internalWindowsFiles = new Dictionary<string, string>();
-        private string winDir;
-        private string sys32;
-        private string sys64;
-        private int myPID;
+        private string _winDir;
+        private string _sys32;
+        private string _sys64;
+        private int _myPID;
         private int myParentPID;
 
         public KillEmAll()
         {
-            winDir = System.IO.Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.System)).ToString().ToLower() + "\\";
-            sys32 = winDir + "system32\\";
-            sys64 = winDir + "syswow64\\";
+            _winDir = System.IO.Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.System)).ToString().ToLower() + "\\";
+            _sys32 = _winDir + "system32\\";
+            _sys64 = _winDir + "syswow64\\";
 
-            myPID = Process.GetCurrentProcess().Id;
-            myParentPID = ParentProcessUtilities.GetParentProcess(myPID).Id;
+            _myPID = Process.GetCurrentProcess().Id;
+            myParentPID = ParentProcessUtilities.GetParentProcess(_myPID).Id;
             
             // these are Windows processes that should not be terminated, full paths
-            string[] temp = { winDir + "explorer.exe", sys32 + "services.exe", sys32 + "winlogon.exe", sys32 + "lsass.exe", sys32 + "logonui.exe", sys32 + "spoolsv.exe",
-                sys32 + "alg.exe", sys32 + "lsm.exe", sys32 + "audiodg.exe", sys32 + "dllhost.exe", sys32 + "msdtc.exe", sys32 + "wscntfy.exe", sys32 + "wudfhost.exe",
-                sys32 + "wininit.exe", sys32 + "mdm.exe", sys32 + "rdpclip.exe", sys32 + "taskmgr.exe", sys32 + "dwm.exe", sys32 + "taskhost.exe", sys32 + "taskeng.exe",
-                sys32 + "sppsvc.exe", sys32 + "conhost.exe", sys32 + "wisptis.exe", sys32 + "tabtip.exe", sys32 + "inputpersonalization.exe", sys32 + "wbem\\wmiprvse.exe",
-                sys64 + "wbem\\wmiprvse.exe", sys32 + "ui0detect.exe", sys32 + "sihost.exe", sys32 + "wlms\\wlms.exe", sys32 + "smss.exe",
-                sys32 + "csrss.exe", sys32 + "svchost.exe", sys64 + "svchost.exe", sys32 + "dashost.exe", sys32 + "runtimebroker.exe", sys32 + "taskhostw.exe",
-                sys32 + "sppsvc.exe", sys32 + "fontdrvhost.exe", sys32 + "systemsettingsbroker.exe", sys32 + "securityhealthservice.exe", sys32 + "sgrmbroker.exe"};
+            string[] temp = { _winDir + "explorer.exe", _sys32 + "services.exe", _sys32 + "winlogon.exe", _sys32 + "lsass.exe", _sys32 + "logonui.exe", _sys32 + "spoolsv.exe",
+                _sys32 + "alg.exe", _sys32 + "lsm.exe", _sys32 + "audiodg.exe", _sys32 + "dllhost.exe", _sys32 + "msdtc.exe", _sys32 + "wscntfy.exe", _sys32 + "wudfhost.exe",
+                _sys32 + "wininit.exe", _sys32 + "mdm.exe", _sys32 + "rdpclip.exe", _sys32 + "taskmgr.exe", _sys32 + "dwm.exe", _sys32 + "taskhost.exe", _sys32 + "taskeng.exe",
+                _sys32 + "sppsvc.exe", _sys32 + "conhost.exe", _sys32 + "wisptis.exe", _sys32 + "tabtip.exe", _sys32 + "inputpersonalization.exe", _sys32 + "wbem\\wmiprvse.exe",
+                _sys64 + "wbem\\wmiprvse.exe", _sys32 + "ui0detect.exe", _sys32 + "sihost.exe", _sys32 + "wlms\\wlms.exe", _sys32 + "smss.exe",
+                _sys32 + "csrss.exe", _sys32 + "svchost.exe", _sys64 + "svchost.exe", _sys32 + "dashost.exe", _sys32 + "runtimebroker.exe", _sys32 + "taskhostw.exe",
+                _sys32 + "sppsvc.exe", _sys32 + "fontdrvhost.exe", _sys32 + "systemsettingsbroker.exe", _sys32 + "securityhealthservice.exe", _sys32 + "sgrmbroker.exe"};
             foreach (string i in temp)
             {
                 try
@@ -48,38 +48,43 @@ namespace KillEmAll.NET
 
         public void Start()
         {
-            IntPtr HANLDE_Processes = CreateToolhelp32SnapshotRtlMoveMemory(2, 0);
+            IntPtr HANLDE_Processes = CreateToolhelp32Snapshot(2, 0);
 
             PROCESSENTRY32W p32Iw = new PROCESSENTRY32W();
             int size = Marshal.SizeOf(typeof(PROCESSENTRY32W));
             p32Iw.dwSize = Convert.ToUInt32(size);
-            bool blFirstProcess = Process32First(HANLDE_Processes, ref p32Iw);
+
+            bool blFirstProcess = Process32FirstW(HANLDE_Processes, ref p32Iw);
             //int x = Marshal.GetLastWin32Error();
             if (blFirstProcess)
             {
                 do
                 {
-                    // get the process ID, filename, and full path
+                    // get the process ID for testing
                     int PID = (int)p32Iw.th32ProcessID;
-                    string filename = p32Iw.szExeFile.ToLower();
-                    string fullpath = getPathFromPID(PID).ToLower();
 
                     // always skip when processID = 0 or 4 which are system critical, or is equal to my processID or my parent process
                     // theory behind skipping the parent process is that it is conhost in a console app, explorer.exe if run by the user
                     // in a GUI based app, or another app in a suite that is using this app as part of it's functionality.
-                    if (PID != 0 && PID != 4 && PID != myPID && PID != myParentPID)
+                    if (PID != 0 && PID != 4 && PID != _myPID && PID != myParentPID)
                     {
-                        // ensure we have a filename string, not sure why this would ever be empty but maybe I ran into it once, I don't recall...
-                        if (!string.IsNullOrEmpty(filename.Trim()))
+                        // check for child processes of this process and skip them, not that KillEmAll.NET does this but other apps I might use this class in might...
+                        // so get the parent process ID and make sure that isn't me.
+                        int ParentPID = (int)p32Iw.th32ParentProcessID;
+                        if (ParentPID != _myPID)
                         {
-                            // another quick check for system critical processes (that aren't actually files with extensions)
-                            if (!processIsSystemCritical(filename))
+                            // get the filename
+                            string filename = p32Iw.szExeFile.ToLower();
+
+                            // ensure we have a filename string, not sure why this would ever be empty but maybe I ran into it once, I don't recall...
+                            if (!string.IsNullOrEmpty(filename.Trim()))
                             {
-                                // check for child processes of this process and skip them, not that KillEmAll.NET does this but other apps I might use this class in might...
-                                // so get the parent process ID and make sure that isn't me.
-                                int ParentPID = (int)p32Iw.th32ParentProcessID;
-                                if (ParentPID != myPID)
+                                // another quick check for system critical processes (that aren't actually files with extensions)
+                                if (!processIsSystemCritical(filename))
                                 {
+                                    // get the full path AFTER the checks above, because getPathFromPID can throw an exception trying to OpenProcess on PID=0, etc.
+                                    string fullpath = getPathFromPID(PID).ToLower();
+
                                     if (fullpath.Contains("\\"))
                                     {
                                         // we have a full path, so check against full path whitelist
@@ -88,11 +93,11 @@ namespace KillEmAll.NET
                                     }
                                     else
                                     {
-                                        // here we didn't obtain a full path, and we're working with just the filename (this is 99.9% of the time a Windows file in a system dir)
+                                        // didn't obtain a full path, working with just the filename (this is 99.9% of the time a Windows file in a system dir)
                                         // I didn't bother to create a second/single EXE whitelist, so prepend a system path and check the whitelist...
-                                        // I don't test for winDir by itself because we always obtain a path from Explorer.exe, and that is the only file from winDir in our whitelist.
-                                        if (!_internalWindowsFiles.ContainsKey(sys32 + filename))
-                                            if (!_internalWindowsFiles.ContainsKey(sys64 + filename))
+                                        // don't test for _winDir because we always obtain a path from Explorer.exe, the only file from _winDir in the whitelist.
+                                        if (!_internalWindowsFiles.ContainsKey(_sys32 + filename))
+                                            if (!_internalWindowsFiles.ContainsKey(_sys64 + filename))
                                                 killProcess(filename, PID);
                                     }
                                 }
@@ -100,13 +105,13 @@ namespace KillEmAll.NET
                         }
                     }
                 }
-                while (Process32Next(HANLDE_Processes, ref p32Iw));
+                while (Process32NextW(HANLDE_Processes, ref p32Iw));
             }
         }
 
         void killProcess(string process, int PID)
         {
-            bool success;
+            bool bSuccess = false;
             //
             // TODO:  add features for optional termination and to google the process
             //
@@ -117,10 +122,9 @@ namespace KillEmAll.NET
             //    success = killProcessByPID(PID);
             //    Console.WriteLine($"Terminated={success}  [{process}]");
             //}
-            //if (foo.KeyChar.ToString().ToLower().Equals("g"))
 
-            success = killProcessByPID(PID);
-            Console.WriteLine($"Terminated={success}  [{process}]");
+            bSuccess = killProcessByPID(PID);
+            Console.WriteLine($"Terminated={bSuccess}  [{process}]");
         }
 
         bool killProcessByPID(int PID)
@@ -156,18 +160,37 @@ namespace KillEmAll.NET
         string getPathFromPID(int PID)
         {
             IntPtr hProcess;
-            bool bSuccess;
+            bool bSuccess = false;
             var buffer = new StringBuilder(1024);
             string sRet = "";
 
+            // this call requires Windows Vista+ due to the flag QueryLimitedInformation,
+            // see https://docs.microsoft.com/en-us/windows/win32/procthread/process-security-and-access-rights
+            // this call should just fail on XP/2003 so flow continues to use the .NET method below.
             hProcess = OpenProcess(ProcessAccessFlags.QueryLimitedInformation, false, PID);
 
             if (hProcess != IntPtr.Zero)
             {
                 int size = buffer.Capacity;
-                bSuccess = QueryFullProcessImageName(hProcess, 0, buffer, ref size);
+                bSuccess = QueryFullProcessImageNameW(hProcess, 0, buffer, ref size);
                 sRet = buffer.ToString();
             }
+            // actually, the .NET method just throws exceptions on access denied and is not successful retrieving any path that fails above...
+            // leaving this code here so I can remember it isn't worth using.
+            //
+            //if (!bSuccess)
+            //{
+            //    // on failure above, do it the .NET way, rather than wrestle with GetModuleFileNameExW in C#
+            //    var process = Process.GetProcessById(PID);
+            //    // below is throwing exception on access denied
+            //    try
+            //    {
+            //        sRet = process.MainModule.FileName;
+            //    }
+            //    catch
+            //    {
+            //    }
+            //}
             return sRet;
         }
 
@@ -186,22 +209,23 @@ namespace KillEmAll.NET
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)] public string szExeFile;
         };
         [DllImport("kernel32.dll", EntryPoint = "CreateToolhelp32Snapshot")]
-        public static extern IntPtr CreateToolhelp32SnapshotRtlMoveMemory(UInt32 dwFlagsdes, UInt32 th32ProcessID);
+        public static extern IntPtr CreateToolhelp32Snapshot(UInt32 dwFlags, UInt32 th32ProcessID);
 
-        [DllImport("kernel32", EntryPoint = "Process32First", SetLastError = true, CharSet = CharSet.Auto)]
+        [DllImport("kernel32.dll", EntryPoint = "Process32FirstW", SetLastError = true, CharSet = CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool Process32First(IntPtr hSnapshot, ref PROCESSENTRY32W lppe);
+        public static extern bool Process32FirstW(IntPtr hSnapshot, ref PROCESSENTRY32W lppe);
 
-        [DllImport("kernel32", EntryPoint = "Process32Next", CharSet = CharSet.Auto)]
+        [DllImport("kernel32.dll", EntryPoint = "Process32NextW", CharSet = CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool Process32Next(IntPtr hSnapshot, ref PROCESSENTRY32W lppe);
+        public static extern bool Process32NextW(IntPtr hSnapshot, ref PROCESSENTRY32W lppe);
 
-        [DllImport("kernel32", EntryPoint = "CloseHandle")]
+        [DllImport("kernel32.dll", EntryPoint = "CloseHandle")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool CloseHandle(IntPtr handle);
 
         [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        static extern bool QueryFullProcessImageName(IntPtr hProcess, uint dwFlags, [Out, MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpExeName, ref int lpdwSize);
+        static extern bool QueryFullProcessImageNameW(IntPtr hProcess, uint dwFlags, [Out, MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpExeName, ref int lpdwSize);
+
         public enum ProcessAccessFlags : uint
         {
             All = 0x001F0FFF,
@@ -218,7 +242,6 @@ namespace KillEmAll.NET
             QueryLimitedInformation = 0x00001000,
             Synchronize = 0x00100000
         }
-
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess, bool bInheritHandle, int dwProcessId);
 
@@ -227,6 +250,14 @@ namespace KillEmAll.NET
         static extern bool TerminateProcess(IntPtr hProcess, uint uExitCode);
     }
 
+
+    // looking for NtQueryInformationProcess usage, I borrowed this struct from Simon's comment (not the accepted answer)
+    // here:  https://stackoverflow.com/questions/394816/how-to-get-parent-process-in-net-in-managed-way
+    // which is supposedly the preferred method for speed in C#, but also because I use the same API in d7x code, and I must have had a good reason.
+    //
+    /// <summary>
+    /// A utility class to determine a process parent.
+    /// </summary>
     [StructLayout(LayoutKind.Sequential)]
     public struct ParentProcessUtilities
     {
